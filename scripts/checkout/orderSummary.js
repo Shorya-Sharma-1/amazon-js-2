6b3671cc1e8e741f406b5d9b0d6a49b2e26020b3
Shorya Sharma <shoryasharma251920@gmail.com>
2026-07-02 01:31:12 +0530

    generate html for payment summary and make it real time

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,7 +1,7 @@
 import {cart, removeFromCart, calculateCartQuantity, updateDeliveryOption} from "../../data/carts.js"
 import {products, getProduct} from "../../data/products.js"
 import {formatCurrency} from '../utils/money.js'
-import {deliveryOptions} from '../../data/deliveryOptions.js'
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
 
@@ -15,14 +15,8 @@ export function renderOrderSummary() {
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let deliveryOption;
-
-    deliveryOptions.forEach((option) => {
-        if (option.id === deliveryOptionId) {
-                deliveryOption = option;
-        }
-
-    });
+    const deliveryOption = getDeliveryOption(deliveryOptionId);
+    
     const today = dayjs();
     const deliveryDate = today.add (
         deliveryOption.deliveryDays, 'days');
