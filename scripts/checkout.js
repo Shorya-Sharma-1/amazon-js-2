aa6c91496529887ba1051c12d7710f056792cde7
Shorya Sharma <shoryasharma251920@gmail.com>
2026-08-11 17:59:44 +0530

    Learn promises and covert code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,11 +1,53 @@
 import {renderOrderSummary} from './checkout/orderSummary.js'
 import {renderPaymentSummary} from './checkout/paymentSummary.js'
 import { loadProducts } from '../data/products.js'
+import { loadCart} from '../data/carts.js'
 //import '../data/backend-practice.js'
 //import '../data/cart-class.js'
 
-loadProducts(()=>{
-    renderPaymentSummary();
-    renderOrderSummary() ;
-
+Promise.all([
+  new Promise ((resolve) => {
+    loadProducts(()=>{
+        resolve('value 1');
+    });
+  }),
+  new Promise ((resolve)=>{
+     loadCart(()=>{
+        resolve('value 2');
+     });
+   })
+]).then((values)=>{
+    console.log(values);
+   renderPaymentSummary();
+    renderOrderSummary();
 })
+
+
+
+/*
+new Promise ((resolve) => {
+    loadProducts(()=>{
+        resolve();
+    });
+}).then(()=>{
+    return new Promise ((resolve)=>{
+        loadCart(()=>{
+           resolve();
+        });
+    });
+}).then(()=>{
+   renderPaymentSummary();
+    renderOrderSummary();
+})
+*/
+
+/*
+
+loadProducts(()=>{
+    loadCart(()=>{
+        renderPaymentSummary();
+        renderOrderSummary() ;
+    });
+});
+    
+*/
