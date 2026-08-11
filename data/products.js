9b840e09d410b5e253257396711714ddda25ed4c
Shorya Sharma <shoryasharma251920@gmail.com>
2026-08-11 11:46:43 +0530

    Load products from the backend

--- data/products.js
+++ data/products.js
@@ -58,10 +58,33 @@ class clothing extends Product {
 
 } 
 
+
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load' , ()=>{
+    products = JSON.parse(xhr.response).map((productDetails)=> {
+      if(productDetails.type === 'clothing'){
+        return new clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log(products);
+
+    fun();
+  });
+
+  xhr.open('GET' , 'https://supersimplebackend.dev/products');
+  xhr.send();
+}
+
+
    
   
 
-
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -728,4 +751,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
-
+*/
