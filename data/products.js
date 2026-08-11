5d1af229e97c2e6e56db401454f969219edb6e18
Shorya Sharma <shoryasharma251920@gmail.com>
2026-08-12 02:03:04 +0530

    Convert loadProducts from XHR to fetch

--- data/products.js
+++ data/products.js
@@ -61,6 +61,28 @@ class clothing extends Product {
 
 export let products = [];
 
+export function loadProductsFetch(){
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+  ).then((response)=>{
+    return response.json();
+  }).then((productsData)=> {
+    products = productsData.map((productDetails)=> {
+      if(productDetails.type === 'clothing'){
+        return new clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log('products');
+  })
+  return promise;
+}
+/*
+loadProductsFetch().then(()=>{
+  console.log('next')
+})
+  */
+
 export function loadProducts(fun) {
   const xhr = new XMLHttpRequest();
 
